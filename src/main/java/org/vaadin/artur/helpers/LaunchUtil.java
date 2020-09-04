fa65be755f8302f4ee2460577cd0db6b7ef46e10
Artur Signell <artur@vaadin.com>
2020-09-04 14:08:26 +0300

    Use npm 'open' to launch a browser without AWT

--- src/main/java/org/vaadin/artur/helpers/LaunchUtil.java
+++ src/main/java/org/vaadin/artur/helpers/LaunchUtil.java
@@ -1,41 +1,42 @@
 package org.vaadin.artur.helpers;
 
-import java.awt.Desktop;
-import java.net.URL;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.server.DevModeHandler;
+import com.vaadin.flow.server.frontend.FrontendTools;
+import com.vaadin.flow.server.frontend.FrontendUtils;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 
+@NpmPackage(value = "open", version = "^7.2.1")
 public class LaunchUtil {
     private static final String LAUNCH_TRACKER = "LaunchUtil.hasLaunched";
-    private static final String HEADLESS_PROPERTY = "java.awt.headless";
 
     private static boolean isProductionMode() {
         return DevModeHandler.getDevModeHandler() == null;
     }
 
     public static void launchBrowser(String location, String alternativeText) {
-
-        String oldProperty = System.getProperty(HEADLESS_PROPERTY);
-        System.setProperty(HEADLESS_PROPERTY, "false");
-
         try {
-            URL url = new URL(location);
-            Desktop desktop = Desktop.getDesktop();
-            desktop.browse(url.toURI());
+            runNodeScript("const open = require('open');open('" + location + "');");
         } catch (Throwable e) {
             LoggerFactory.getLogger(LaunchUtil.class).info(alternativeText);
-        } finally {
-            if (oldProperty == null) {
-                System.clearProperty(HEADLESS_PROPERTY);
-            } else {
-                System.setProperty(HEADLESS_PROPERTY, oldProperty);
-            }
-
         }
+    }
 
+    private static int runNodeScript(String script) throws InterruptedException, IOException {
+        FrontendTools tools = new FrontendTools("", () -> FrontendUtils.getVaadinHomeDirectory().getAbsolutePath());
+        String node = tools.getNodeExecutable();
+        List<String> command = new ArrayList<>();
+        command.add(node);
+        command.add("-e");
+        command.add(script);
+        ProcessBuilder builder = FrontendUtils.createProcessBuilder(command);
+        return builder.start().waitFor();
     }
 
     private static boolean isLaunched() {
