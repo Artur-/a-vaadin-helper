51380f117d53b09001fcff88cb04a98beafda364
Artur <artur@vaadin.com>
2020-10-16 11:05:59 +0300

    Add sorting support based on TypeScript sorting information (#3)

--- src/main/java/org/vaadin/artur/helpers/PagingUtil.java
+++ src/main/java/org/vaadin/artur/helpers/PagingUtil.java
@@ -1,6 +1,5 @@
 package org.vaadin.artur.helpers;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,12 +19,10 @@ public class PagingUtil {
         //
     }
 
-    private static PageRequest limitAndOffsetAndSortOrdersToPageSizeAndNumber(int offset, int limit, List<SortOrder<String>> sortOrders) {
+    private static PageRequest limitAndOffsetAndSortOrdersToPageSizeAndNumber(int offset, int limit, Sort sort) {
         int minPageSize = limit;
         int lastIndex = offset + limit - 1;
         int maxPageSize = lastIndex + 1;
-        List<Order> orders =  sortOrders.stream().map(PagingUtil::vaadinSortOrderToSpringOrder).collect(Collectors.toList());
-        Sort sort = Sort.by(orders);
         for (double pageSize = minPageSize; pageSize <= maxPageSize; pageSize++) {
             int startPage = (int) (offset / pageSize);
             int endPage = (int) (lastIndex / pageSize);
@@ -34,23 +31,54 @@ public class PagingUtil {
                 return PageRequest.of(startPage, (int) pageSize, sort);
             }
         }
-        
+
         // Should not really get here
         return PageRequest.of(0, maxPageSize, sort);
     }
 
-    private static Order vaadinSortOrderToSpringOrder(SortOrder<String> sortOrder){
+    private static Order javaSortOrderToSpringOrder(SortOrder<String> sortOrder) {
         Direction direction = sortOrder.getDirection() == SortDirection.ASCENDING ? Direction.ASC : Direction.DESC;
         return new Order(direction, sortOrder.getSorted());
+    }
+
+    private static Sort javaSortToSpring(List<SortOrder<String>> sortOrders) {
+        List<Order> orders = sortOrders.stream().map(PagingUtil::javaSortOrderToSpringOrder)
+                .collect(Collectors.toList());
+        return Sort.by(orders);
+    }
+
+    private static Sort tsSortToSpring(List<GridSorter> sortOrders) {
+        List<Order> orders = sortOrders.stream().map(PagingUtil::tsSorterToSpringOrder).collect(Collectors.toList());
+        return Sort.by(orders);
+    }
+
+    private static Order tsSorterToSpringOrder(GridSorter gridSorter) {
+        String dir = gridSorter.getDirection().orElse("asc");
+        Direction direction = dir.equals("asc") ? Direction.ASC : Direction.DESC;
+        return new Order(direction, gridSorter.getPath());
     }
 
     public static Pageable offsetLimitToPageable(int offset, int limit) {
         return offsetLimitSortOrdersToPageable(offset, limit, Collections.emptyList());
     }
 
+    public static Pageable offsetLimitTypeScriptSortOrdersToPageable(int offset, int limit,
+            List<GridSorter> sortOrders) {
+        Sort sort = tsSortToSpring(sortOrders);
+
+        PageRequest pageSizeAndNumberAndSortOrders = limitAndOffsetAndSortOrdersToPageSizeAndNumber(offset, limit,
+                sort);
+        return PageRequest.of(pageSizeAndNumberAndSortOrders.getPageNumber(),
+                pageSizeAndNumberAndSortOrders.getPageSize(), pageSizeAndNumberAndSortOrders.getSort());
+    }
+
     public static Pageable offsetLimitSortOrdersToPageable(int offset, int limit, List<SortOrder<String>> sortOrders) {
-        PageRequest pageSizeAndNumberAndSortOrders = limitAndOffsetAndSortOrdersToPageSizeAndNumber(offset, limit, sortOrders);
-        return PageRequest.of(pageSizeAndNumberAndSortOrders.getPageNumber(), pageSizeAndNumberAndSortOrders.getPageSize(), pageSizeAndNumberAndSortOrders.getSort());
+        Sort sort = javaSortToSpring(sortOrders);
+        PageRequest pageSizeAndNumberAndSortOrders = limitAndOffsetAndSortOrdersToPageSizeAndNumber(offset, limit,
+                sort);
+
+        return PageRequest.of(pageSizeAndNumberAndSortOrders.getPageNumber(),
+                pageSizeAndNumberAndSortOrders.getPageSize(), pageSizeAndNumberAndSortOrders.getSort());
     }
 
 }
